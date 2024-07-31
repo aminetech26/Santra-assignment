95ceb291ae6b2d874a7e4e1dccb287f2c84b0ae6
aminetech26 <la_guerraiche@esi.dz>
2024-07-31 17:44:43 +0200

    feat: Register blocs for all sellers, home slider, shop card, user area, top sellers, and categories

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -2,7 +2,12 @@ import 'package:get_it/get_it.dart';
 import 'package:santra_assignment/data/data_sources/local_data_source.dart';
 import 'package:santra_assignment/data/repositories/local_data_repo_impl.dart';
 import 'package:santra_assignment/domain/repositories/local_data_repo.dart';
+import 'package:santra_assignment/presentation/blocs/all_sellers/all_sellers_bloc.dart';
 import 'package:santra_assignment/presentation/blocs/categories/categories_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/home_slider/home_slider_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/shop_card/shop_card_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/top_sellers/top_sellers_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/user_area/user_area_bloc.dart';
 
 final getItInstance = GetIt.I;
 
@@ -13,4 +18,14 @@ Future initDependencies() async {
       () => LocalDataSourceImpl());
   getItInstance.registerFactory(
       () => CategoriesBloc(localDataRepo: getItInstance()));
+  getItInstance.registerFactory(
+  () => AllSellersBloc(localDataRepo: getItInstance()));
+  getItInstance.registerFactory(
+      () => TopSellersBloc(localDataRepo: getItInstance()));
+  getItInstance.registerFactory(
+  () => HomeSliderBloc(localDataRepo: getItInstance()));
+  getItInstance.registerFactory(
+      () => ShopCardBloc(localDataRepo: getItInstance()));
+  getItInstance.registerFactory(
+      () => UserAreaBloc(localDataRepo: getItInstance()));
 }
--- lib/presentation/journeys/home_screen.dart
+++ lib/presentation/journeys/home_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:santra_assignment/di/get_it.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/all_sellers/all_sellers_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/categories/categories_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/home_slider/home_slider_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/shop_card/shop_card_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/top_sellers/top_sellers_bloc.dart';
+import 'package:santra_assignment/presentation/blocs/user_area/user_area_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +16,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late AllSellersBloc _allSellersBloc;
+  late TopSellersBloc _topSellersBloc;
+  late CategoriesBloc _categoriesBloc;
+  late HomeSliderBloc _homeSliderBloc;
+  late ShopCardBloc _shopCardBloc;
+  late UserAreaBloc _userAreaBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _allSellersBloc = getItInstance<AllSellersBloc>();
+    _topSellersBloc = getItInstance<TopSellersBloc>();
+    _categoriesBloc = getItInstance<CategoriesBloc>();
+    _homeSliderBloc = getItInstance<HomeSliderBloc>();
+    _shopCardBloc = getItInstance<ShopCardBloc>();
+    _userAreaBloc = getItInstance<UserAreaBloc>();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _allSellersBloc.close();
+    _topSellersBloc.close();
+    _categoriesBloc.close();
+    _homeSliderBloc.close();
+    _shopCardBloc.close();
+    _userAreaBloc.close();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => _allSellersBloc,
+        ),
+        BlocProvider(
+          create: (context) => _categoriesBloc,
+        ),
+      ],
+      child: Placeholder(),
+    );
   }
 }
